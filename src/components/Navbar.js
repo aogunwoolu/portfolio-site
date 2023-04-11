a6d2c4b70d764cee6c981fdd174b08b3fc8d0362
Abi <a.ogunwoolu@outlook.com>
2023-04-11 23:25:24 +0100

    feat: general fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,20 +2,6 @@ import { Link } from 'gatsby';
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 
-// export default function Navbar() {
-//   return (
-//   <nav className='mynav'>
-//     <h1></h1>
-//     <div className="links">
-//         <Link to="#startPage">Home</Link>
-//         <Link to="#radar">About</Link>
-//         <Link to="#projects">Projects</Link>
-//         <Link to="/">Resume</Link>
-//     </div>
-//   </nav>
-//   )
-// }
-
 export default function Navbar() {
   const [open, setOpen] = useState(false);
 
@@ -24,7 +10,12 @@ export default function Navbar() {
   };
 
   return (
-    <nav className='mynav'>
+    <nav
+      initial={false}
+      animate={open ? "open" : "closed"}
+      className="menu"
+      className='mynav'
+    >
       <h1></h1>
       <div className={`hidden md:mx-8 md:block md:flex md:justify-end`}>
         <Link to="#startPage">Home</Link>
--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,72 +1,69 @@
-import React, { useState }  from 'react';
+import React, { useState, useEffect } from 'react';
 import { Col, Container, Row, Button } from 'react-bootstrap';
 import * as styles from '../styles/start.module.css';
 import { FaGithub } from 'react-icons/fa';
-import { AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai'
-import { BsChevronDown } from 'react-icons/bs'
-import Navbar from './Navbar'
+import { AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai';
+import { BsChevronDown } from 'react-icons/bs';
+import Navbar from './Navbar';
 import ContactFormModal from './ContactForm';
 
 export default function StartPage({id}) {
-const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div id={id}>
-    <div className={styles.nebula_img}>
+      <div className={styles.nebula_img}>
         <Container className={styles.about_container}>
-            <Row>
-                <Col className={styles.about_me}>
-                    <div className={styles.content}>
-                        <h3>
-                            <strong>Hey, I'm</strong>
-                        </h3>
-                        <h1>
-                            <p><span className="highlight">Abi</span>(sade),</p>
-                        </h1>
-                        <h3>
-                            <strong>Full-Stack Developer.</strong><br/>
-                            <strong>I also do DevOps & AI things.</strong>
-                        </h3>
-                        <h3>
-                            <strong className="skill-array">
-                                [Frontend,Backend,Databases] 
-                            </strong>
-                        </h3>
-                        <br/>
-                        <Button onClick={()=>{setIsOpen(true)}} variant="flat">Contact Me</Button>
-                        <br/>
-                        <Container>
-                            <Row className={styles.social_bar}>
-                                <Col>
-                                    <FaGithub onClick={() => window.open("https://github.com/aogunwoolu")} size={28}/>
-                                </Col>
-                                <Col>
-                                    <AiFillTwitterCircle onClick={() => window.open("https://github.com/aogunwoolu")} size={28}/>
-                                </Col>
-                                <Col>
-                                    <AiFillLinkedin onClick={() => window.open("https://www.linkedin.com/in/abisade-ogunwoolu-0bb804155/")} size={28}/>
-                                </Col>
-                            </Row>
-                        </Container>
-                    </div>
-                </Col>
-                <Col className='col-4'/>
-            </Row>
-            <Row className={styles.scroll}>
-                <Col className='col-1'>
-                    <BsChevronDown size={28}/>
-                </Col>
-                <Col className='col-1'/>
-                <Col>
-                    <p className='align-middle'>scroll for more</p>
-                </Col>
-            </Row>
+          <Row>
+            <Col className={styles.about_me}>
+              <div className={styles.content}>
+                <h3>
+                  <strong>Hey, I'm</strong>
+                </h3>
+                <h1>
+                  <p><span className="highlight">Abi</span>(sade),</p>
+                </h1>
+                <h3>
+                  <strong>Full-Stack Developer.</strong><br/>
+                  <strong>I also do DevOps & AI things.</strong>
+                </h3>
+                <h3>
+                  <strong className="skill-array">
+                    [Frontend,Backend,Databases] 
+                  </strong>
+                </h3>
+                <br/>
+                <Button onClick={()=>{setIsOpen(true)}} variant="flat">Contact Me</Button>
+                <br/>
+                <Container>
+                  <Row className={styles.social_bar}>
+                    <Col>
+                      <FaGithub className="icon" onClick={() => window.open("https://github.com/aogunwoolu")} size={28}/>
+                    </Col>
+                    <Col>
+                      <AiFillTwitterCircle className="icon" onClick={() => window.open("https://github.com/aogunwoolu")} size={28}/>
+                    </Col>
+                    <Col>
+                      <AiFillLinkedin className="icon" onClick={() => window.open("https://www.linkedin.com/in/abisade-ogunwoolu-0bb804155/")} size={28}/>
+                    </Col>
+                  </Row>
+                </Container>
+              </div>
+            </Col>
+            <Col className='col-4'/>
+          </Row>
+          <Row className={styles.scroll}>
+            <Col className='col-1'>
+              <BsChevronDown size={28}/>
+            </Col>
+            <Col className='col-1'/>
+            <Col>
+              <p className='align-middle'>scroll for more</p>
+            </Col>
+          </Row>
         </Container>
-        {/* <Container className='bottom-0'>
-            
-        </Container> */}
-    </div>
-    <ContactFormModal isOpen={isOpen} setIsOpen={setIsOpen}/>
+      </div>
+      <ContactFormModal isOpen={isOpen} setIsOpen={setIsOpen}/>
     </div>
   );
 }
