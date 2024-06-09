fef488245827931b1da669d4bdf122bc8b44e6f8
Abi <a.ogunwoolu@outlook.com>
2024-06-09 19:59:28 +0100

    fix: fixed incorrect email

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import { Col, Container, Row, Button } from 'react-bootstrap';
-import * as styles from '../styles/start.module.css';
-import { FaGithub } from 'react-icons/fa';
-import { AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai';
-import { BsChevronDown } from 'react-icons/bs';
-import ContactFormModal from './ContactForm';
+import React, { useState } from "react"
+import { Col, Container, Row, Button } from "react-bootstrap"
+import * as styles from "../styles/start.module.css"
+import { FaGithub } from "react-icons/fa"
+import { AiFillTwitterCircle, AiFillLinkedin } from "react-icons/ai"
+import { BsChevronDown } from "react-icons/bs"
+import ContactFormModal from "./ContactForm"
 
-export default function StartPage({id}) {
-  const [isOpen, setIsOpen] = useState(false);
+export default function StartPage({ id }) {
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <div id={id}>
@@ -20,51 +20,72 @@ export default function StartPage({id}) {
                   <strong>Hey, I'm</strong>
                 </h3>
                 <h1>
-                  <p><span className="highlight">Abi</span>(sade),</p>
+                  <p>
+                    <span className="highlight">Abi</span>(sade),
+                  </p>
                 </h1>
                 <h3>
-                  <strong>Full-Stack Developer.</strong><br/>
+                  <strong>Full-Stack Developer.</strong>
+                  <br />
                   <strong>I also do DevOps & AI things.</strong>
                 </h3>
                 <h3>
                   <strong className="skill-array">
-                    [Frontend, Backend, Databases] 
+                    [Frontend, Backend, Databases]
                   </strong>
                 </h3>
-                <br/>
-                <a href="mailto:a.gounwoolu@outlook.com">
+                <br />
+                <a href="mailto:a.ogunwoolu@outlook.com">
                   <Button variant="flat">Contact Me</Button>
                 </a>
-                <br/>
+                <br />
                 <Container>
                   <Row className={styles.social_bar}>
                     <Col>
-                      <FaGithub className="icon" onClick={() => window.open("https://github.com/aogunwoolu")} size={28}/>
+                      <FaGithub
+                        className="icon"
+                        onClick={() =>
+                          window.open("https://github.com/aogunwoolu")
+                        }
+                        size={28}
+                      />
                     </Col>
                     <Col>
-                      <AiFillTwitterCircle className="icon" onClick={() => window.open("https://twitter.com/home")} size={28}/>
+                      <AiFillTwitterCircle
+                        className="icon"
+                        onClick={() => window.open("https://twitter.com/home")}
+                        size={28}
+                      />
                     </Col>
                     <Col>
-                      <AiFillLinkedin className="icon" onClick={() => window.open("https://www.linkedin.com/in/abisade-ogunwoolu-0bb804155/")} size={28}/>
+                      <AiFillLinkedin
+                        className="icon"
+                        onClick={() =>
+                          window.open(
+                            "https://www.linkedin.com/in/abisade-ogunwoolu-0bb804155/"
+                          )
+                        }
+                        size={28}
+                      />
                     </Col>
                   </Row>
                 </Container>
               </div>
             </Col>
-            <Col className='col-4'/>
+            <Col className="col-4" />
           </Row>
           <Row className={styles.scroll}>
-            <Col className='col-1'>
-              <BsChevronDown size={28}/>
+            <Col className="col-1">
+              <BsChevronDown size={28} />
             </Col>
-            <Col className='col-1'/>
+            <Col className="col-1" />
             <Col>
-              <p className='align-middle'>scroll for more</p>
+              <p className="align-middle">scroll for more</p>
             </Col>
           </Row>
         </Container>
       </div>
       {/* <ContactFormModal isOpen={isOpen} setIsOpen={setIsOpen}/> */}
     </div>
-  );
+  )
 }
